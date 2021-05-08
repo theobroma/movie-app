959b06877d3e7096cad69fb8a27db2de20f91b2b
Alexandr Siryj <theobroma222@gmail.com>
2021-05-08 15:46:22 +0300

    backdrop

--- src/@components/UI/LoadingPage/LoadingPage.tsx
+++ src/@components/UI/LoadingPage/LoadingPage.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { CircularProgress, Backdrop, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 export const LoadingPage: React.FC = () => {
-  return <div>Loading...</div>;
+  const classes = useStyles();
+  return (
+    <Backdrop className={classes.backdrop} open>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
 };
 export default LoadingPage;
