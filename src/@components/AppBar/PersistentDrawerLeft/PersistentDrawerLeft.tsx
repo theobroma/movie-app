a151d3b9d209a915d292d9fee880a637c7566acf
Alexandr Siryj <theobroma222@gmail.com>
2021-12-22 12:38:43 +0200

    PersistentDrawerLeft start

---
+++ src/@components/AppBar/PersistentDrawerLeft/PersistentDrawerLeft.tsx
@@ -0,0 +1,116 @@
+import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { useTheme } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import clsx from 'clsx';
+import React from 'react';
+import { useStyles } from './PersistentDrawerLeft.styles';
+
+const PersistentDrawerLeft: React.FC = ({ children }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div
+      className={clsx({
+        [classes.root]: true,
+        'HolyGrail-content': true,
+      })}
+      // className={classes.root}
+    >
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default PersistentDrawerLeft;
--- src/@routes/AppLayout.tsx
+++ src/@routes/AppLayout.tsx
@@ -2,20 +2,32 @@ import { Box } from '@material-ui/core';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../@components/Footer';
-import PersistentDrawerLeft from '../@components/AppBar';
+import AppBar from '../@components/AppBar';
+import PersistentDrawerLeft from '../@components/AppBar/PersistentDrawerLeft/PersistentDrawerLeft';
 
 export const AppLayout = () => {
   return (
     <div className="HolyGrail">
       <Box>
-        <PersistentDrawerLeft />
+        <AppBar />
       </Box>
       <main className="HolyGrail-content">
         <Outlet />
       </main>
-      {/* Test WDYR */}
-      {/* <ComponentWithProblem /> */}
       <Footer />
     </div>
   );
 };
+
+// export const AppLayout = () => {
+//   return (
+//     <div className="HolyGrail">
+//       <PersistentDrawerLeft>
+//         <Outlet />
+//       </PersistentDrawerLeft>
+//       {/* Test WDYR */}
+//       {/* <ComponentWithProblem /> */}
+//       <Footer />
+//     </div>
+//   );
+// };
