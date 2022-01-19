ba339b13163c319e828288f173a46772dfff51a3
Alexandr Siryj <theobroma222@gmail.com>
2022-01-19 06:49:04 +0200

    rootReducer with slices

--- src/@store/configureStore.ts
+++ src/@store/configureStore.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, Reducer } from '@reduxjs/toolkit';
+import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import {
   persistStore,
@@ -11,7 +12,15 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { rootReducer } from './index';
+import { similarSlice } from '../@features/SimilarMedia/store/slice';
+import { userSlice } from './user/slice';
+import { trendingSlice } from './trending/slice';
+import { searchSlice } from './search/slice';
+import { entitiesSlice } from './entities/slice';
+import { detailsSlice } from './details/slice';
+import { uiSlice } from './ui/slice';
+import { moviesSlice } from './movies/slice';
+// import { rootReducer } from './index';
 
 const logger = createLogger({
   collapsed: true,
@@ -24,8 +33,30 @@ const persistConfig = {
   whitelist: ['ui', 'user'], // will be persisted
 };
 
+const reducers = {
+  [detailsSlice.name]: detailsSlice.reducer,
+  [entitiesSlice.name]: entitiesSlice.reducer,
+  [moviesSlice.name]: moviesSlice.reducer,
+  [searchSlice.name]: searchSlice.reducer,
+  [similarSlice.name]: similarSlice.reducer,
+  [trendingSlice.name]: trendingSlice.reducer,
+  [uiSlice.name]: uiSlice.reducer,
+  [userSlice.name]: userSlice.reducer,
+  // [anyApi.reducerPath]: anyApi.reducer,
+};
+
+const combinedReducer = combineReducers<typeof reducers>(reducers);
+
+export const rootReducer: Reducer<RootState> = (state, action) => {
+  //   if (action.type === RESET_STATE_ACTION_TYPE) {
+  //     state = {} as RootState;
+  //   }
+
+  return combinedReducer(state, action);
+};
+
 // Middleware: Redux Persist Persisted Reducer
-const persistedReducer = persistReducer(persistConfig, rootReducer());
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -41,3 +72,11 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 export default { store, persistor };
+
+// ==================== TYPES ====================
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof combinedReducer>;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
