e47f08368e4ee71850dde6df16e50c31b5b06035
Alexandr Siryj <theobroma222@gmail.com>
2022-01-20 11:21:12 +0200

    TrendingTVView FC

--- src/@views/TrendingView/TrendingTVView/TrendingTVView.tsx
+++ src/@views/TrendingView/TrendingTVView/TrendingTVView.tsx
@@ -1,40 +1,56 @@
+import { Container, Grid } from '@material-ui/core';
+import { nanoid } from '@reduxjs/toolkit';
 import * as React from 'react';
+import { useQueryParam, NumberParam } from 'use-query-params';
+import SingleContent from '../../../@components/SingleContent';
+import SingleContentSkeleton from '../../../@components/SingleContent/SingleContentSkeleton';
+import CustomPagination from '../../../@components/UI/CustomPagination';
 import { useListPostsQuery } from '../../../@store/trending/api';
 
 const TrendingTVView = () => {
-  const [page, setPage] = React.useState(1);
-  const { data, isLoading, isFetching } = useListPostsQuery(page);
+  const [queryPage, setQueryPage] = useQueryParam('page', NumberParam);
+  const { data, isLoading, isFetching } = useListPostsQuery(queryPage || 1);
 
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
+  console.log('isLoading', isLoading);
+  console.log('isFetching', isFetching);
 
-  if (!data?.results) {
-    return <div>No posts :(</div>;
-  }
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    pageValue: number,
+  ) => {
+    if (pageValue !== 1) {
+      setQueryPage(pageValue);
+    } else {
+      setQueryPage(undefined);
+    }
+    // Scroll to top when page changes
+    window.scroll(0, 0);
+  };
 
   return (
-    <div>
-      {data.results.map(({ id, title, status }: any) => (
-        <div key={id}>
-          {title} - {status}
-        </div>
-      ))}
-      <button
-        type="button"
-        onClick={() => setPage(page - 1)}
-        // isLoading={isFetching}
-      >
-        Previous
-      </button>
-      <button
-        type="button"
-        onClick={() => setPage(page + 1)}
-        // isLoading={isFetching}
-      >
-        Next
-      </button>
-    </div>
+    <Container maxWidth="lg">
+      <Grid container spacing={3} style={{ padding: 3 }}>
+        {data?.results.map((tv) => (
+          <Grid item xs={12} sm={4} md={3} lg={2} key={nanoid()}>
+            {isFetching ? (
+              <SingleContentSkeleton />
+            ) : (
+              <SingleContent movie={tv} />
+            )}
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container spacing={3} style={{ padding: 3 }}>
+        <Grid item xs={12}>
+          <CustomPagination
+            onChange={handlePageChange}
+            count={data?.total_pages}
+            // page={data?.page}
+            page={queryPage || 1}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
