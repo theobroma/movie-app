bc1b77501b52e6deb736ee6ac98600dab77adf19
Alexandr Siryj <theobroma222@gmail.com>
2022-03-31 01:50:52 +0300

    rename locales folder

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,8 +4,8 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-import translationEN from './locales/en/translation.json';
-import translationRU from './locales/ru/translation.json';
+import translationEN from './@locales/en/translation.json';
+import translationRU from './@locales/ru/translation.json';
 
 const resources = {
   en: {
@@ -29,7 +29,6 @@ i18n
       escapeValue: false,
     },
     debug: true,
-    // loadPath: '/locales/{{lng}}/translation.json',
   });
 
 export default i18n;
