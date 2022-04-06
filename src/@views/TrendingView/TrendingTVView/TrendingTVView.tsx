1e3d50e5d14b43abaac4735ced44619ae19e2409
Alexandr Siryj <theobroma222@gmail.com>
2022-04-07 02:16:23 +0300

    trending tv with i18n

--- src/@views/TrendingView/TrendingTVView/TrendingTVView.tsx
+++ src/@views/TrendingView/TrendingTVView/TrendingTVView.tsx
@@ -1,18 +1,24 @@
 import { Box, Container, Grid, Typography } from '@material-ui/core';
 import { nanoid } from '@reduxjs/toolkit';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import { NumberParam, useQueryParam } from 'use-query-params';
 import SingleContent from '../../../@components/SingleContent';
 import SingleContentSkeleton from '../../../@components/SingleContent/SingleContentSkeleton';
 import CustomPagination from '../../../@components/UI/CustomPagination';
 import { useTrendingTVQuery } from '../../../@store/trending/api';
+import { languageSelector } from '../../../@store/ui/selectors';
+import { alpha2iso } from '../../../@utils/alpha2iso';
 
 const TrendingTVView = () => {
   const [queryPage, setQueryPage] = useQueryParam('page', NumberParam);
-  const { data, isLoading, isFetching } = useTrendingTVQuery(queryPage || 1);
+  const currentLanguage = useSelector(languageSelector);
+  const langISOCode = alpha2iso(currentLanguage);
 
-  // console.log('isLoading', isLoading);
-  // console.log('isFetching', isFetching);
+  const { data, isFetching } = useTrendingTVQuery({
+    page: queryPage || 1,
+    isoCode: langISOCode,
+  });
 
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
